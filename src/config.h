743b37bbf0fd4b3b678bd4233bc62661f0c14ecd
RJM <ryan@errantgames.com>
2014-12-29 21:20:06 -0500

    Quick and dirty sprite animation using classes from SFML github wiki (attributions noted in source per the instructions).

--- src/config.h
+++
@@ -1,2 +0,0 @@
-#define perseverance_VERSION_MAJOR 1
-#define perseverance_VERSION_MINOR 0
--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,162 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <tmx/MapLoader.h>
+#include <Animation.hpp>
+#include <AnimatedSprite.hpp>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "Version " << perseverance_VERSION_MAJOR << "." << perseverance_VERSION_MINOR << endl;
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "myproject");
+    sf::Vector2i screenDimensions(800, 600);
+    sf::RenderWindow window(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Perseverance");
+    window.setFramerateLimit(60);
 
     tmx::MapLoader ml("resources/maps");
     ml.AddSearchPath("resources/maps/atlas");
     ml.Load("landing.tmx");
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("resources/sprites/body/male/tanned.png")) {
+    sf::Texture spriteSheet;
+    if (!spriteSheet.loadFromFile("resources/sprites/complete/male_default.png")) {
         return -1;
     }
 
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
+    Animation walkingAnimationUp;
+    walkingAnimationUp.setSpriteSheet(spriteSheet);
+    walkingAnimationUp.addFrame(sf::IntRect(64, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(128, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(192, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(256, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(320, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(384, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(448, 512, 64, 64));
+    walkingAnimationUp.addFrame(sf::IntRect(512, 512, 64, 64));
+
+    Animation standingAnimationUp;
+    standingAnimationUp.setSpriteSheet(spriteSheet);
+    standingAnimationUp.addFrame(sf::IntRect(0, 512, 64, 64));
+
+    Animation walkingAnimationLeft;
+    walkingAnimationLeft.setSpriteSheet(spriteSheet);
+    walkingAnimationLeft.addFrame(sf::IntRect(64, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(128, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(192, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(256, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(320, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(384, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(448, 576, 64, 64));
+    walkingAnimationLeft.addFrame(sf::IntRect(512, 576, 64, 64));
+
+    Animation standingAnimationLeft;
+    standingAnimationLeft.setSpriteSheet(spriteSheet);
+    standingAnimationLeft.addFrame(sf::IntRect(0, 576, 64, 64));
+
+    Animation walkingAnimationDown;
+    walkingAnimationDown.setSpriteSheet(spriteSheet);
+    walkingAnimationDown.addFrame(sf::IntRect(64, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(128, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(192, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(256, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(320, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(384, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(448, 640, 64, 64));
+    walkingAnimationDown.addFrame(sf::IntRect(512, 640, 64, 64));
+
+    Animation standingAnimationDown;
+    standingAnimationDown.setSpriteSheet(spriteSheet);
+    standingAnimationDown.addFrame(sf::IntRect(0, 640, 64, 64));
+
+    Animation walkingAnimationRight;
+    walkingAnimationRight.setSpriteSheet(spriteSheet);
+    walkingAnimationRight.addFrame(sf::IntRect(64, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(128, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(192, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(256, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(320, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(384, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(448, 704, 64, 64));
+    walkingAnimationRight.addFrame(sf::IntRect(512, 704, 64, 64));
+
+    Animation standingAnimationRight;
+    standingAnimationRight.setSpriteSheet(spriteSheet);
+    standingAnimationRight.addFrame(sf::IntRect(0, 704, 64, 64));
+
+    Animation* currentAnimation = &standingAnimationDown;
+    Animation* lastStandingHeadingAnimation = &standingAnimationDown;
+
+    AnimatedSprite animatedSprite(sf::seconds(0.2), true, false);
+    animatedSprite.setPosition(sf::Vector2f(screenDimensions / 2));
+
+    sf::Clock frameClock;
+
+    float speed = 80.f;
+    bool noKeyWasPressed = true;
 
     while (window.isOpen()) {
-        sf::Event Event;
-        while (window.pollEvent(Event)) {
-            if (Event.type == sf::Event::Closed)
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (sf::Event::Closed == event.type) {
                 window.close();
+            } else if (sf::Event::KeyPressed == event.type
+                    && sf::Keyboard::Escape == event.key.code) {
+                window.close();
+            }
         }
+        sf::Time frameTime = frameClock.restart();
+
+        sf::Vector2f movement(0.f, 0.f);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            currentAnimation = &walkingAnimationUp;
+            lastStandingHeadingAnimation = &standingAnimationUp;
+            movement.y -= speed;
+            noKeyWasPressed = false;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            currentAnimation = &walkingAnimationDown;
+            lastStandingHeadingAnimation = &standingAnimationDown;
+            movement.y += speed;
+            noKeyWasPressed = false;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            currentAnimation = &walkingAnimationLeft;
+            lastStandingHeadingAnimation = &standingAnimationLeft;
+            movement.x -= speed;
+            noKeyWasPressed = false;
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            currentAnimation = &walkingAnimationRight;
+            lastStandingHeadingAnimation = &standingAnimationRight;
+            movement.x += speed;
+            noKeyWasPressed = false;
+        }
+
+        //If no key, use a standing animation that reflects that most recent direction traveled.
+        if (noKeyWasPressed) {
+            currentAnimation = lastStandingHeadingAnimation;
+        }
+
+        animatedSprite.play(*currentAnimation);
+        animatedSprite.move(movement * frameTime.asSeconds());
+
+        if (noKeyWasPressed) {
+            animatedSprite.stop();
+        }
+
+        noKeyWasPressed = true;
+
+        animatedSprite.update(frameTime);
+
+        window.clear();
+//        ml.Draw(window, tmx::MapLayer::DrawType::All, true);
+        ml.Draw(window, 0, true);
+        ml.Draw(window, 1, true);
+        window.draw(animatedSprite);
+        ml.Draw(window, 2, true);
         window.display();
-//        window.draw(sprite);
-        window.draw(ml);
     }
 }
