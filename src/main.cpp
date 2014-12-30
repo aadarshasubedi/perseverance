5f8e26fd08d2d38eeab0eff45e5cbbff6b9c5586
RJM <ryan@errantgames.com>
2014-12-29 21:47:54 -0500

    Scrolling camera added.

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,15 @@ int main(int argc, char *argv[]) {
     cout << "Version " << perseverance_VERSION_MAJOR << "." << perseverance_VERSION_MINOR << endl;
 
     sf::Vector2i screenDimensions(800, 600);
+
     sf::RenderWindow window(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Perseverance");
     window.setFramerateLimit(60);
 
+    //Main view
+    sf::View overworldView(sf::Vector2f(400, 300), sf::Vector2f(screenDimensions.x, screenDimensions.y));
+      overworldView.setViewport(sf::FloatRect(0, 0, 1, 1));
+
+    //Map loading
     tmx::MapLoader ml("resources/maps");
     ml.AddSearchPath("resources/maps/atlas");
     ml.Load("landing.tmx");
@@ -91,7 +97,7 @@ int main(int argc, char *argv[]) {
 
     sf::Clock frameClock;
 
-    float speed = 80.f;
+    float speed = 90.f;
     bool noKeyWasPressed = true;
 
     while (window.isOpen()) {
@@ -102,6 +108,8 @@ int main(int argc, char *argv[]) {
             } else if (sf::Event::KeyPressed == event.type
                     && sf::Keyboard::Escape == event.key.code) {
                 window.close();
+            } else if (sf::Event::Resized == event.type) {
+                overworldView.setSize(event.size.width, event.size.height);
             }
         }
         sf::Time frameTime = frameClock.restart();
@@ -144,6 +152,9 @@ int main(int argc, char *argv[]) {
         animatedSprite.play(*currentAnimation);
         animatedSprite.move(movement * frameTime.asSeconds());
 
+        //Camera tracks sprite
+        overworldView.setCenter(animatedSprite.getPosition());
+
         if (noKeyWasPressed) {
             animatedSprite.stop();
         }
@@ -153,7 +164,7 @@ int main(int argc, char *argv[]) {
         animatedSprite.update(frameTime);
 
         window.clear();
-//        ml.Draw(window, tmx::MapLayer::DrawType::All, true);
+        window.setView(overworldView);
         ml.Draw(window, 0, true);
         ml.Draw(window, 1, true);
         window.draw(animatedSprite);
